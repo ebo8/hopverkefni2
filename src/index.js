8cc7824a21ce8cc05533b21daef395681e2fb964
Arsol Olafsdottir <arsololafsdottir@Arsols-MacBook.local>
2019-11-13 18:23:54 +0000

    header added

--- src/index.js
+++ src/index.js
@@ -4,7 +4,14 @@ document.addEventListener('DOMContentLoaded', () => {
   const page = document.querySelector('body');
   const isLecturePage = page.classList.contains('lecture-page');
 
+  const url = new URL(window.location);
+  const param = new URLSearchParams(url.search);
+  console.log(page);
+  console.log(param);
+  console.log(url);
+
   if (isLecturePage) {
+    console.log('hér');
 
   } else {
     const list = new List();
