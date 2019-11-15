c2c7c1355d15aab5af0ea9a5ffc560e3ba1568e7
Arsol Olafsdottir <arsololafsdottir@Arsols-MacBook.local>
2019-11-15 14:19:21 +0000

    lecture sida

--- src/lib/list.js
+++ src/lib/list.js
@@ -58,20 +58,82 @@ export default class List {
     const video = this.createElement('iframe', 'video', 'none');
     video.src = link;
     div.appendChild(video);
-    console.log(this);
     main.appendChild(div);
+  }
+
+  makeText(text) {
+    const main = document.querySelector('main');
+    const textValue = this.createElement('p', 'lectureText', text);
+    main.appendChild(textValue);
+  }
+
+  makeImage(image, caption) {
+    const main = document.querySelector('main');
+    const figure = this.createElement('figure', 'lectureImg', 'none');
+    const img = this.createElement('img', 'lectureImg', 'none');
+    img.src = image;
+    const imgcap = this.createElement('figcaption', 'lectureImg', caption);
+    figure.appendChild(img);
+    figure.appendChild(imgcap);
+    main.appendChild(figure);
+  }
+
+  makeQuote(quote, attribute) {
+    const main = document.querySelector('main');
+    const quoteContainer = this.createElement('div', 'quote', 'none');
+    const quoteText = this.createElement('p', 'quote--text', quote);
+    const cite = this.createElement('q', 'quote--cite', attribute);
+    quoteContainer.appendChild(quoteText);
+    quoteContainer.appendChild(cite);
+    main.appendChild(quoteContainer);
+  }
+
+  makelectureHeading(text) {
+    const main = document.querySelector('main');
+    const heading = this.createElement('h2', 'lectureh2', text);
+    main.appendChild(heading);
+  }
+
+  makeList(data) {
+    const main = document.querySelector('main');
+    const list = this.createElement('ul', 'lectureList', 'none');
+    for(let i = 0; i < data.length; i += 1) {
+      const listElement = this.createElement('li', 'lectureList--li', data[i]);
+      list.appendChild(listElement);
+    }
+    main.appendChild(list);
+  }
+
+  makeCode(data) {
+    const main = document.querySelector('main');
+    const code = this.createElement('code', 'lectureCode', data);
+    main.appendChild(code);
   }
 
   displayLecture(data) {
     console.log(this.lectureName);
-    console.log(data.lectures[0].content);
 
     for(let i = 0; i < data.lectures.length; i += 1) {
       if(data.lectures[i].slug === this.lectureName) {
         this.makeheader(data.lectures[i].title, data.lectures[i].category, data.lectures[i].image);
-      if(data.lectures[i].content[i].type === 'youtube') {
-        this.makeYoutube(data.lectures[i].content[i].data);
-      }
+        for(let j = 0; j < data.lectures.length; j += 1) {
+          console.log(data.lectures[i].content[j]);
+          if(data.lectures[i].content[j].type === 'youtube') {
+            this.makeYoutube(data.lectures[i].content[j].data);
+          }else if (data.lectures[i].content[j].type === 'text') {
+            this.makeText(data.lectures[i].content[j].data);
+          }else if (data.lectures[i].content[j].type === 'image') {
+            this.makeImage(data.lectures[i].content[j].data, data.lectures[i].content[j].caption);
+          }else if (data.lectures[i].content[j].type === 'quote') {
+            this.makeQuote(data.lectures[i].content[j].data, data.lectures[i].content[j].attribute);
+          }else if (data.lectures[i].content[j].type === 'heading') {
+            this.makelectureHeading(data.lectures[i].content[j].data);
+          }else if (data.lectures[i].content[j].type === 'list') {
+            this.makeList(data.lectures[i].content[j].data);
+          }else if (data.lectures[i].content[j].type === 'code') {
+            this.makeCode(data.lectures[i].content[j].data);
+          }
+        }
       }
     }
   }
